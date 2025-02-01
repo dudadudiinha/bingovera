59f55d276b0c8d9954dda4ba0958f7b4162f9945
carolcostaoliv <Carolinaoliveiraacosta@gmail.com>
2025-01-31 21:03:36 -0300

    botei a função pra criar as cartela, ainda falta mostrar elas e deixar alinhado

--- bingo.py
+++ bingo.py
@@ -1,5 +1,7 @@
 # bingo_python
 
+import random
+
 def modo():
     modo = 2
     while modo != 0 and modo != 1:
@@ -13,5 +15,23 @@ def modo():
     if modo == 1:
         quant_cartelas = 4
         linhas, colunas = 3, 4
-        intervalo = [(1, 10), (11, 20), (21, 30), (31, 40)] 
+        intervalo = [(1, 10), (11, 20), (21, 30), (31, 40)]
         return quant_cartelas, linhas, colunas, intervalo
+    
+def criar_cartelas(quant_cartelas, linhas, colunas, intervalo):
+    cartelas = []
+    for utilidade0 in range(quant_cartelas):
+        cartela = []
+        for coluna in range(colunas):
+            inicio, fim = intervalo[coluna]
+            numeros_coluna = []
+            while len(numeros_coluna) < linhas:
+                num = random.randint(inicio, fim)
+                if num not in numeros_coluna:
+                    numeros_coluna.append(num)
+            for linha in range(linhas):
+                if len(cartela) <= linha:
+                    cartela.append([])
+                cartela[linha].append(numeros_coluna[linha])
+        cartelas.append(cartela)
+    return cartelas, quant_cartelas
